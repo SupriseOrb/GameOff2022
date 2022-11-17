2632f882fd254dddc28143f0e484b37e60462eb7
robtunstall <81332650+robtunstall@users.noreply.github.com>
2022-11-16 16:33:07 -0800

    Title Music Implemented

--- GameOff/Assets/Scripts/Menu/MainMenu.cs
+++ GameOff/Assets/Scripts/Menu/MainMenu.cs
@@ -7,6 +7,9 @@ public class MainMenu : Menu
     public void Start()
     {
         // Rob TODO: Play Main Menu Music
+        AkSoundEngine.SetState("Music_State", "Title");
+        AkSoundEngine.SetState("Battle_Intensity", "Battle_Intensity_1");
+        AkSoundEngine.PostEvent("Music", gameObject);
         // Note: There may be a bug where music duplicates
     }
     public void StartGame()
