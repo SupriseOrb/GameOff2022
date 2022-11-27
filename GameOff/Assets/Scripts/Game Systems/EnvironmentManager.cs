b6e743ea8b0c22981ead89471621070429cd7b9e
Molina <molinakmey@gmail.com>
2022-11-27 15:04:32 -0800

    Block out lighting for 3 intensities

---
+++ GameOff/Assets/Scripts/Game Systems/EnvironmentManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] lighting;
+    
+    public void ChangeIntensity(int currentIntensity)
+    {
+        
+    }
+}
